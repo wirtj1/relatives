d5703a1746bebd37e1bc82eaa1c9fb34e2ca3a6d
SaschaPWittwer <sascha.wittwer@bluewin.ch>
2017-04-25 19:58:10 +0200

    Add example combobox

--- src/main/java/com/vaadin/MainView.java
+++ src/main/java/com/vaadin/MainView.java
@@ -11,6 +11,7 @@ import com.vaadin.ui.Label;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.ComboBox;
 
 /**
  * This UI is the application entry point. A UI may either represent a browser window 
@@ -20,7 +21,7 @@ import com.vaadin.ui.VerticalLayout;
  * overridden to add component to the user interface and initialize non-component functionality.
  */
 @Theme("mytheme")
-public class MyUI extends UI {
+public class MainView extends UI {
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
@@ -29,19 +30,21 @@ public class MyUI extends UI {
         final TextField name = new TextField();
         name.setCaption("Type your name here:");
 
+        ComboBox combo = new ComboBox("Sex");
+        combo.setItems(new String[]{"Male", "Female", "Yes baby.... (lele wtf)"});
+        combo.addValueChangeListener(e -> layout.addComponent(new Label("Are you really a " + e.getValue() + "?")));
+
         Button button = new Button("Click Me");
-        button.addClickListener( e -> {
-            layout.addComponent(new Label("Thanks " + name.getValue() 
-                    + ", it works!"));
-        });
+        button.addClickListener( e -> layout.addComponent(new Label("Thanks " + name.getValue()
+                + ", it works!")));
         
-        layout.addComponents(name, button);
+        layout.addComponents(name, combo, button);
         
         setContent(layout);
     }
 
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
-    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
+    @VaadinServletConfiguration(ui = MainView.class, productionMode = false)
     public static class MyUIServlet extends VaadinServlet {
     }
 }
