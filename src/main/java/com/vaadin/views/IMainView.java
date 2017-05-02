23cc041ad3223e46a01fe731187a68abd739f7b7
Leandro <leandro.lerena@gmail.com>
2017-05-02 16:45:54 +0200

    removed explicit public definition

--- src/main/java/com/vaadin/views/IMainView.java
+++ src/main/java/com/vaadin/views/IMainView.java
@@ -4,11 +4,11 @@ package com.vaadin.views;
  * Created by Sascha on 01/05/2017.
  */
 public interface IMainView {
-    public void setUsername(String username);
+    void setUsername(String username);
 
     interface IMainViewListener {
         void logout();
     }
 
-    public void addListener(IMainViewListener listener);
+    void addListener(IMainViewListener listener);
 }
