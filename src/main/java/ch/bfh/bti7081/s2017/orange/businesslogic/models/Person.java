62893da956a08cfed624dc3451bedb5ba4a03395
Yves Beutler <yves.beutler@hotmail.com>
2017-05-10 13:02:26 +0200

    Feature/repository (#5)
    * implement repository for person entity
    create additional base entity class
    
    * Merged with entities
    
    * fix persistence name
    
    * chore
    
    * finish person repository
    
    * create tests for person repository

--- src/main/java/ch/bfh/bti7081/s2017/orange/businesslogic/models/Person.java
+++ src/main/java/ch/bfh/bti7081/s2017/orange/businesslogic/models/Person.java
@@ -1,23 +1,18 @@
 package ch.bfh.bti7081.s2017.orange.businesslogic.models;
 
+import ch.bfh.bti7081.s2017.orange.Identity;
+import java.io.Serializable;
 import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
 @Inheritance
 @DiscriminatorColumn(name="persontype")
-public class Person implements Serializable{
+
+public class Person extends Identity implements Serializable {
     private String firstName;
     private String lastName;
     private String socialAssuranceNumber;
-
-    @Id
-    @GeneratedValue
-    private Long id;
-
-    public Long getId() {
-        return id;
-    }
 
     public String getFirstName() {
         return firstName;
@@ -30,7 +25,7 @@ public class Person implements Serializable{
     public String getSocialAssuranceNumber() {
         return socialAssuranceNumber;
     }
-
+  
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
