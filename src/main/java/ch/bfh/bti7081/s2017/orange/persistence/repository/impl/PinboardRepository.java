4ae53fee4e3432d2015740f5b312c5ca57ca94e7
Leandro <leandro.lerena@gmail.com>
2017-05-30 20:47:40 +0200

    add pinboard repository

---
+++ src/main/java/ch/bfh/bti7081/s2017/orange/persistence/repository/impl/PinboardRepository.java
@@ -0,0 +1,15 @@
+package ch.bfh.bti7081.s2017.orange.persistence.repository.impl;
+
+import ch.bfh.bti7081.s2017.orange.persistence.entity.PinBoard;
+import ch.bfh.bti7081.s2017.orange.persistence.repository.Repository;
+
+/**
+ * @author Leandro on 30/5/2017.
+ */
+public class PinboardRepository extends Repository<PinBoard> {
+    public PinboardRepository(Class<PinBoard> entityClass) {
+        super(entityClass);
+    }
+
+
+}
